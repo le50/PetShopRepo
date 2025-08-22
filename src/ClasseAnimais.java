a07aa6ce64467aa8d102c6b29d80645c134403e1
le50 <leti.tec1000@gmail.com>
2025-08-22 10:31:13 -0300

    Adicionado Classe pai e filhas

---
+++ src/ClasseAnimais.java
@@ -0,0 +1,24 @@
+
+public class ClasseAnimais {
+
+	String especieAt;
+	String nomeAt;
+	int idadeAt;
+	float precoAt;
+	String corAt;
+	
+	// Construtor
+	public ClasseAnimais() {
+		
+	}
+	
+	// Construtor
+	public ClasseAnimais(String especiePar, String nomePar, int idadePar, float precoPar, String corPar) {
+		
+		this.especieAt = especiePar;
+		this.nomeAt = nomePar;
+		this.idadeAt = idadePar;
+		this.precoAt = precoPar;
+		this.corAt = corPar;
+	}
+}
